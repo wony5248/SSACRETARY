f215da769415855f093a47fbc53d3e63811588b5
ssoonD <sxxnweek@gmail.com>
2021-10-18 09:17:06 +0900

    Test Controller 추가, SwaggerConfig 추가

---
+++ backend/src/main/java/com/ssacretary/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.ssacretary.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("SSACRETARY")
+                .description("SSACRETARY API")
+                .build();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
+                .apiInfo(this.apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                ;
+    }
+
+}
