5a53b15eef4df0370d4fd638c225cf538a268b19
장범진 <qjawls0501@naver.com>
2021-10-20 17:13:11 +0900

    feat:router add

--- backend/src/main/java/com/ssacretary/SsacretaryApplication.java
+++ backend/src/main/java/com/ssacretary/SsacretaryApplication.java
@@ -2,12 +2,16 @@ package com.ssacretary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SsacretaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsacretaryApplication.class, args);
+
 	}
 
 }
+
