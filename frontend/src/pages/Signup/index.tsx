b6b5e72bfa118af830f5abbdb508a2053f02bde3
OTKRyu <appn12@gmail.com>
2021-10-21 16:49:36 +0900

    feat: basic signup page before adding function and style

--- frontend/src/pages/Signup/index.tsx
+++ frontend/src/pages/Signup/index.tsx
@@ -1,39 +1,42 @@
 import React from "react";
-import styled from "styled-components";
-import AAtag from "../../components/Atag";
-import { useMediaQuery } from "react-responsive";
-import AppAppBar from "../../views/AppAppBar";
-import { Headerdiv, Bodydiv, Settingdiv } from "../Settingprofile/style";
-const Desktop = ({ children }: any) => {
-  const isDesktop = useMediaQuery({ minWidth: 613 });
-  return isDesktop ? children : null;
-};
-const Tablet = ({ children }: any) => {
-  const isTablet = useMediaQuery({ minWidth: 613, maxWidth: 1060 });
-  return isTablet ? children : null;
-};
-const Mobile = ({ children }: any) => {
-  const isMobile = useMediaQuery({ maxWidth: 612 });
-  return isMobile ? children : null;
-};
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./index.css";
+
 const SignUp = () => {
-  const isMobile = useMediaQuery({ maxWidth: 612 });
   return (
     <div>
-      <AppAppBar />
-      <Desktop>this page is SignUp page</Desktop>
-      <Mobile>
-        <div>
-            <Headerdiv>Crawlings</Headerdiv>
-            <Bodydiv>
-                <Settingdiv></Settingdiv>
-                <Settingdiv></Settingdiv>
-                <Settingdiv></Settingdiv>
-                <Settingdiv></Settingdiv>
-                <Settingdiv></Settingdiv>
-            </Bodydiv>
-            </div>this page is SignUp page111
-      </Mobile>
+      <h1 className="headline">SIGN UP</h1>
+      <div>
+        <TextField name="email" label="Email" required={true} />
+        <Button name="email" variant="contained">
+          CHECK
+        </Button>
+      </div>
+      <div>
+        <TextField name="nickname" label="Nickname" required={true} />
+        <Button name="nickname" variant="contained">
+          CHECK
+        </Button>
+      </div>
+      <div>
+        <TextField name="passwordCheck" label="Password" required={true} />
+      </div>
+      <div>
+        <TextField name="passwordCheck" label="PasswordCheck" required={true} />
+      </div>
+      <div>
+        <TextField name="phone" label="Phone" />
+        <Button name="phone" variant="contained">
+          CHECK
+        </Button>
+      </div>
+      <div>
+        <Button variant="contained">SIGN UP</Button>
+      </div>
+      <div>
+        <Button variant="outlined">GO BACK</Button>
+      </div>
     </div>
   );
 };
