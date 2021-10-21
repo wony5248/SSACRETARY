d097637864fc57a3499d3b0983bb3314dba74a6d
OTKRyu <appn12@gmail.com>
2021-10-21 16:25:12 +0900

    feat: basic login page before function

--- frontend/src/pages/Signin/index.tsx
+++ frontend/src/pages/Signin/index.tsx
@@ -1,31 +1,44 @@
-import React from 'react';
-import styled from 'styled-components';
-import AAtag from "../../components/Atag"
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
+const SignIn = () => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = inputs;
+  const onChange = (event: any) => {
+    const { value, name } = event.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
 
-const SignIn =  () => {
-    return(
-        <div>
-            this page is SignIn page
-            <br/>
-            <AAtag href="/" name = "SignIn"></AAtag>
-            <br/>
-            <AAtag href="/signup" name = "SignUp"></AAtag>
-            <br/>
-            <AAtag href="/changecrawl" name = "ChangeCrawl"></AAtag>
-            <br/>
-            <AAtag href="/makecrawl" name = "MakeCrawl"></AAtag>
-            <br/>
-            <AAtag href="/log" name = "Log"></AAtag>
-            <br/>
-            <AAtag href="/settingprofile" name = "SettingProfile"></AAtag>
-            <br/>
-            <AAtag href="/specificcrawling" name = "Specificcrawling"></AAtag>
-            <br/>
-            <AAtag href="/userprofile" name = "UserProfile"></AAtag>
-        </div>
-    );
+  const onSignIn = function () {};
+  const onSignUp = function () {};
+  return (
+    <div>
+      <h1 className="headline">SIGN IN</h1>
+      <div>
+        <TextField label="Email" />
+      </div>
+      <div>
+        <TextField label="Password" />
+      </div>
+      <div>
+        <Button variant="contained" color="primary">
+          SING IN
+        </Button>
+      </div>
+      <div>
+        <Button variant="outlined" color="primary">
+          CREATE ACCOUNT
+        </Button>
+      </div>
+    </div>
+  );
 };
-
 
 export default SignIn;
