ea3a6c5be19800b37861449b6c11b0abd6c2e222
장범진 <qjawls0501@naver.com>
2021-10-21 17:18:13 +0900

    feat:settingprofile add

--- frontend/src/pages/Settingprofile/index.tsx
+++ frontend/src/pages/Settingprofile/index.tsx
@@ -1,31 +1,64 @@
-import React from 'react';
-import styled from 'styled-components';
-import AAtag from "../../components/Atag"
-
-
-const SettingProfile =  () => {
-    return(
-        <div>
-            this page is SettingProfile page
-            <br/>
-            <AAtag href="/" name = "SignIn"></AAtag>
-            <br/>
-            <AAtag href="/signup" name = "SignUp"></AAtag>
-            <br/>
-            <AAtag href="/changecrawl" name = "ChangeCrawl"></AAtag>
-            <br/>
-            <AAtag href="/makecrawl" name = "MakeCrawl"></AAtag>
-            <br/>
-            <AAtag href="/log" name = "Log"></AAtag>
-            <br/>
-            <AAtag href="/settingprofile" name = "SettingProfile"></AAtag>
-            <br/>
-            <AAtag href="/specificcrawling" name = "Specificcrawling"></AAtag>
-            <br/>
-            <AAtag href="/userprofile" name = "UserProfile"></AAtag>
-        </div>
-    );
+import React from "react";
+import styled from "styled-components";
+import AAtag from "../../components/Atag";
+import { useMediaQuery } from "react-responsive";
+import AppAppBar from "../../views/AppAppBar";
+import {
+  Headerdiv,
+  Bodydiv,
+  Settingdiv,
+  Settingtitlediv,
+  Settingtagdiv,
+  Settingtag
+} from "./style";
+import Btn from "../../components/Button";
+const Desktop = ({ children }: any) => {
+  const isDesktop = useMediaQuery({ minWidth: 613 });
+  return isDesktop ? children : null;
+};
+const Tablet = ({ children }: any) => {
+  const isTablet = useMediaQuery({ minWidth: 613, maxWidth: 1060 });
+  return isTablet ? children : null;
+};
+const Mobile = ({ children }: any) => {
+  const isMobile = useMediaQuery({ maxWidth: 612 });
+  return isMobile ? children : null;
 };
 
+const SettingProfile = () => {
+  const Click = () => {
+    console.log("clicked");
+  };
+  return (
+    <div>
+      <AppAppBar />
+      <Desktop>this page is SettingProfile page</Desktop>
+      <Mobile>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Headerdiv>Crawlings</Headerdiv>
+          <Bodydiv>
+            <Settingdiv>
+              <Settingtitlediv>Title1</Settingtitlediv>
+              <Settingtagdiv><Settingtag>tag1</Settingtag></Settingtagdiv>
+            </Settingdiv>
+            <Settingdiv></Settingdiv>
+            <Settingdiv></Settingdiv>
+            <Settingdiv></Settingdiv>
+            <Settingdiv></Settingdiv>
+            <Settingdiv></Settingdiv>
+            <Settingdiv></Settingdiv>
+          </Bodydiv>
+          <Btn onClick={Click} name="Make Crawl"></Btn>
+        </div>
+      </Mobile>
+    </div>
+  );
+};
 
 export default SettingProfile;
--- frontend/src/pages/Specificcrawling/index.tsx
+++ frontend/src/pages/Specificcrawling/index.tsx
@@ -1,30 +1,95 @@
-import React from 'react';
-import styled from 'styled-components';
-import AAtag from "../../components/Atag"
-
-const SpecificCrawling =  () => {
-    return(
-        <div>
-            this page is SpecificCrawling page
-            <br/>
-            <AAtag href="/" name = "SignIn"></AAtag>
-            <br/>
-            <AAtag href="/signup" name = "SignUp"></AAtag>
-            <br/>
-            <AAtag href="/changecrawl" name = "ChangeCrawl"></AAtag>
-            <br/>
-            <AAtag href="/makecrawl" name = "MakeCrawl"></AAtag>
-            <br/>
-            <AAtag href="/log" name = "Log"></AAtag>
-            <br/>
-            <AAtag href="/settingprofile" name = "SettingProfile"></AAtag>
-            <br/>
-            <AAtag href="/specificcrawling" name = "Specificcrawling"></AAtag>
-            <br/>
-            <AAtag href="/userprofile" name = "UserProfile"></AAtag>
-        </div>
-    );
+import React from "react";
+import styled from "styled-components";
+import AAtag from "../../components/Atag";
+import Box from "@mui/material/Box";
+import { useMediaQuery } from "react-responsive";
+import AppAppBar from "../../views/AppAppBar";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Urldiv, Carddiv, Cardcondiv } from "./style";
+import Btn from "../../components/Button";
+import { Grid } from "@mui/material";
+const Desktop = ({ children }: any) => {
+  const isDesktop = useMediaQuery({ minWidth: 613 });
+  return isDesktop ? children : null;
+};
+const Tablet = ({ children }: any) => {
+  const isTablet = useMediaQuery({ minWidth: 613, maxWidth: 1060 });
+  return isTablet ? children : null;
+};
+const Mobile = ({ children }: any) => {
+  const isMobile = useMediaQuery({ maxWidth: 612 });
+  return isMobile ? children : null;
 };
 
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
 
-export default SpecificCrawling;
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Word of the Day
+      </Typography>
+      <Typography variant="h5" component="div">
+        be{bull}nev{bull}o{bull}lent
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        adjective
+      </Typography>
+    </CardContent>
+    <CardActions></CardActions>
+  </React.Fragment>
+);
+
+const SettingProfile = () => {
+  const Click = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <div>
+      <AppAppBar />
+      <Desktop>this page is SettingProfile page</Desktop>
+      <Mobile>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Urldiv>URL</Urldiv>
+          <Carddiv>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Card variant="outlined">{card}</Card>
+              </Grid>
+              <Grid item xs={6}>
+                <Card variant="outlined">{card}</Card>
+              </Grid>
+              <Grid item xs={6}>
+                <Card variant="outlined">{card}</Card>
+              </Grid>
+              <Grid item xs={6}>
+                <Card variant="outlined">{card}</Card>
+              </Grid>
+            </Grid>
+          </Carddiv>
+          <Btn onClick={Click} name="Go to Detail"></Btn>
+          <Btn onClick={Click} name="Make Crawl"></Btn>
+        </div>
+      </Mobile>
+    </div>
+  );
+};
+
+export default SettingProfile;
