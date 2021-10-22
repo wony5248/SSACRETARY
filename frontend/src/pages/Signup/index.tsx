3775eace8904d86094b1b8a48933e422f212658c
OTKRyu <appn12@gmail.com>
2021-10-22 11:59:25 +0900

    feat: add input update and chenge check to availity check

--- frontend/src/pages/Signup/index.tsx
+++ frontend/src/pages/Signup/index.tsx
@@ -1,35 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import "./index.css";
 
 const SignUp = () => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    nickname: "",
+    password: "",
+    passwordCheck: "",
+    phone: "",
+  });
+
+  const { email, nickname, password, passwordCheck, phone } = inputs;
+
+  const onChange = (event: any) => {
+    const { value, name } = event.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const onEmailCheck = () => {};
+  const onNicknameCheck = () => {};
+  const onPhoneCheck = () => {};
+  const onSingUp = () => {};
+  const onGoBack = () => {};
+
   return (
     <div>
       <h1 className="headline">SIGN UP</h1>
       <div>
-        <TextField name="email" label="Email" required={true} />
+        <TextField
+          name="email"
+          label="Email"
+          required={true}
+          onChange={onChange}
+        />
         <Button name="email" variant="contained">
-          CHECK
+          AVAILITY CHECK
         </Button>
       </div>
       <div>
-        <TextField name="nickname" label="Nickname" required={true} />
+        <TextField
+          name="nickname"
+          label="Nickname"
+          required={true}
+          onChange={onChange}
+        />
         <Button name="nickname" variant="contained">
-          CHECK
+          AVAILITY CHECK
         </Button>
       </div>
       <div>
-        <TextField name="passwordCheck" label="Password" required={true} />
+        <TextField
+          name="password"
+          label="Password"
+          required={true}
+          onChange={onChange}
+        />
       </div>
       <div>
-        <TextField name="passwordCheck" label="PasswordCheck" required={true} />
+        <TextField
+          error={password === passwordCheck ? false : true}
+          name="passwordCheck"
+          label="PasswordCheck"
+          required={true}
+          onChange={onChange}
+          helperText={
+            password === passwordCheck
+              ? ""
+              : "passwordCheck isn't identical to password"
+          }
+        />
       </div>
       <div>
-        <TextField name="phone" label="Phone" />
-        <Button name="phone" variant="contained">
-          CHECK
-        </Button>
+        <TextField name="phone" label="Phone" onChange={onChange} />
+        <Button variant="contained">AVAILITY CHECK</Button>
       </div>
       <div>
         <Button variant="contained">SIGN UP</Button>
