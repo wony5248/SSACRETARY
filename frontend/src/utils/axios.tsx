fc4866d431be984ce39d46b8c3cd056f60b25218
OTKRyu <appn12@gmail.com>
2021-10-25 16:06:57 +0900

    feat: add axios functions

--- frontend/src/utils/axios.tsx
+++ frontend/src/utils/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { BASE_URL } from "./statics";
 
-const axiosOnSignIn = function (email: String, password: String) {
+export const axiosOnSignIn = function (email: String, password: String) {
   return axios({
     method: "POST",
     url: BASE_URL + "/user/login",
@@ -12,4 +12,24 @@ const axiosOnSignIn = function (email: String, password: String) {
   });
 };
 
-export { axiosOnSignIn };
+export const axiosOnEmailCheck = function (email: String) {
+  console.log(BASE_URL + "/userEmailCheck/" + email);
+  return axios({
+    method: "GET",
+    url: BASE_URL + "/userEmailCheck/" + email,
+  });
+};
+
+export const axiosOnNicknameCheck = function (nickname: String) {
+  return axios({
+    method: "GET",
+    url: BASE_URL + "/userNickNameCheck/" + nickname,
+  });
+};
+
+export const axiosOnPhoneNumberCheck = function (phone: String) {
+  return axios({
+    method: "GET",
+    url: BASE_URL + "/userPhoneNumberCheck/" + phone,
+  });
+};
