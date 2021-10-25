e7853f9da04b515f30574d2b5b2119fe83e7ff4c
장범진 <qjawls0501@naver.com>
2021-10-25 10:54:07 +0900

    fix:conflict fix

--- frontend/src/pages/Signin/index.tsx
+++ frontend/src/pages/Signin/index.tsx
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import AAtag from "../../components/Atag";
-import { useMediaQuery } from "react-responsive";
-import AppAppBar from "../../views/AppAppBar";
-const Desktop = ({ children }: any) => {
-  const isDesktop = useMediaQuery({ minWidth: 613 });
-  return isDesktop ? children : null;
-};
-const Tablet = ({ children }: any) => {
-  const isTablet = useMediaQuery({ minWidth: 613, maxWidth: 1060 });
-  return isTablet ? children : null;
-};
-const Mobile = ({ children }: any) => {
-  const isMobile = useMediaQuery({ maxWidth: 612 });
-  return isMobile ? children : null;
-};
-
-const SignIn = () => {
-  const isMobile = useMediaQuery({ maxWidth: 612 });
-  return (
-    <div>
-      <AppAppBar />
-      <Desktop>this page is SignIn page</Desktop>
-      <Mobile>this page is SignIn page111</Mobile>
-=======
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -83,7 +57,6 @@ const SignIn: React.FunctionComponent = () => {
           CREATE ACCOUNT
         </Button>
       </Link>
->>>>>>> d173eae38493e2694a05d487d1c583b4caddb38a
     </div>
   );
 };
