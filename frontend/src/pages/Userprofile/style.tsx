fd407e7d6b2179b60eb612dcb120681d1b6e5aaa
장범진 <qjawls0501@naver.com>
2021-10-25 10:49:41 +0900

    fix:conflict

---
+++ frontend/src/pages/Userprofile/style.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Profilediv = styled.div`
+    display:flex;
+    align-items: center;
+    margin-top: 24px;
+    margin-bottom: 24px;
+`;
+export const Profileinput = styled.input`
+    display : flex;
+    flex-direction: column;
+    align-items:center;
+    height: 450px;
+    width: 90%;
+    overflow: auto;
+    border-radius: 8px;
+    // border : 1px solid black;
+`;
+export const Duplicate = styled.div`
+    border: 1px solid #6200EE;
+    width: 360px;
+    border-radius: 8px;
+    margin-top:12px;
+    margin-bottom: 12px;
+    min-height: 88px;
+`;
