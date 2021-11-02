a59768c0eed35162bfb4e61c56905ab61218078f
장범진 <qjawls0501@naver.com>
2021-11-02 14:50:42 +0900

    feat:email add

--- backend/src/main/java/com/ssacretary/ScheduledTasks.java
+++ backend/src/main/java/com/ssacretary/ScheduledTasks.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,11 +17,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+
 @Component
 public class ScheduledTasks {
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 60000)
     public void getStockPriceList() {
-
+        final String useremail = "wony5248@gmail.com";
+        final String password = "hjvqhqcmqjjyhenj";
+        StringBuffer contents = new StringBuffer();
         final String stockList = "https://cafe.naver.com/ArticleList.nhn?search.clubid=27907035&search.boardtype=L&search.totalCount=151&search.cafeId=27907035&search.page=1";
 //        final String stockList = "https://finance.naver.com/sise/sise_market_sum.nhn?&page=1";
         Connection conn = Jsoup.connect(stockList);
@@ -36,6 +47,7 @@ public class ScheduledTasks {
             while (true){
                 boolean matfind = matcher.find();
                 if (matfind){
+
                     int i = matcher.start();
                     int start = 0;
                     int end = 0;
@@ -69,6 +81,7 @@ public class ScheduledTasks {
                     }
                     System.out.println(doctext.substring(start, end));
                     arr.add(doctext.substring(start, end));
+                    contents.append(doctext.substring(start, end));
                 }
                 else{
                     break;
@@ -76,6 +89,48 @@ public class ScheduledTasks {
             }
 //            System.out.println(doctext);
             System.out.println(cnt);
+            String subject = "새로운 크롤링 결과";
+            String fromMail = "wony5248@gmail.com";
+            String fromName = "장범진";
+            String toMail = "wony5248@gmail.com";
+            Properties props = new Properties();
+
+            props.put("mail.smtp.host", "smtp.gmail.com"); // use Gmail
+            props.put("mail.smtp.port", "587"); // set port
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true"); // use TLS
+
+
+            Session mailSession = Session.getInstance(props, new javax.mail.Authenticator(){
+                protected PasswordAuthentication getPasswordAuthentication(){
+                    return new PasswordAuthentication(useremail, password);
+                }
+            });
+
+            try {
+                MimeMessage message = new MimeMessage(mailSession);
+
+                message.setFrom(new InternetAddress(fromMail, MimeUtility.encodeText(fromName, "UTF-8", "B")));
+                message.setRecipients(
+                        Message.RecipientType.TO,
+                        InternetAddress.parse(toMail)
+                );
+                message.setSubject(subject);
+                message.setContent(cnt + "개의 크롤링 조건에 일치하는 결과" + contents.toString(),"text/html;charset=UTF-8");
+                message.setSentDate(new java.util.Date());
+
+                Transport t = mailSession.getTransport("smtp");
+                t.connect(useremail, password);
+                t.sendMessage(message, message.getAllRecipients());
+                t.close();
+                System.out.println("DONE");
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+
+
 //            System.out.println(tbody);
 
 
