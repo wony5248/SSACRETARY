3e9f676d4f9758d828b288489983bb738b70b0e1
jihyun9623 <jihyun9623@gmail.com>
2021-11-05 11:52:26 +0900

    Cors설정 3

--- backend/src/main/java/com/ssacretary/SsacretaryApplication.java
+++ backend/src/main/java/com/ssacretary/SsacretaryApplication.java
@@ -1,18 +1,41 @@
 package com.ssacretary;
 
+import io.swagger.models.HttpMethod;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @EnableScheduling
 @SpringBootApplication
-public class SsacretaryApplication implements WebMvcConfigurer {
+public class SsacretaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsacretaryApplication.class, args);
 
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedOrigins("http://front-server.com");
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:8080")
+						.allowedOrigins("http://localhost:3000")
+						.allowedOrigins("http://ssacretary.com")
+						.allowedMethods(
+								HttpMethod.GET.name(),
+								HttpMethod.HEAD.name(),
+								HttpMethod.POST.name(),
+								HttpMethod.PUT.name(),
+								HttpMethod.DELETE.name());
+			}
+		};
 	}
 
 }
