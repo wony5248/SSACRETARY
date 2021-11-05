469dafac34e2813b55385969553d1e4f0882fc07
jihyun9623 <jihyun9623@gmail.com>
2021-11-05 10:30:08 +0900

    Feat : Status Code 변경, 주석 정리

--- backend/src/main/java/com/ssacretary/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/ssacretary/api/service/UserServiceImpl.java
@@ -4,13 +4,10 @@ import com.ssacretary.api.request.user.EditUserReq;
 import com.ssacretary.api.request.user.LoginReq;
 import com.ssacretary.api.request.user.SignupReq;
 import com.ssacretary.api.response.user.UserLoginPostRes;
-import com.ssacretary.common.response.BaseResponseBody;
 import com.ssacretary.config.JwtTokenProvider;
 import com.ssacretary.db.entity.User;
 import com.ssacretary.db.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -25,43 +22,25 @@ public class UserServiceImpl implements UserService{
     JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public boolean isValidToken(String token) {
-        if (token != null && token.length() > 0) {
-            //logger.debug("token 검증");
-            return jwtTokenProvider.validateToken(token);
-        } else {
-            throw new RuntimeException("인증 토큰이 없습니다");
+    public boolean createUser(SignupReq signupReq) {
+        try{
+            if (signupReq.getPassword().equals(signupReq.getPasswordCheck())) {
+                userRepository.save(User.builder()
+                        .email(signupReq.getEmail())
+                        .nickname(signupReq.getNickname())
+                        .phone(signupReq.getPhone())
+                        .password(passwordEncoder.encode(signupReq.getPassword()))
+                        .build());
+            }
+            return true;
+        }catch (Exception e){
+            System.out.println(e);
+            return false;
         }
     }
-
-    @Override
-    public void createUser(SignupReq signupReq) {
-        if (signupReq.getPassword().equals(signupReq.getPasswordCheck())) {
-            userRepository.save(User.builder()
-                    .email(signupReq.getEmail())
-                    .nickname(signupReq.getNickname())
-                    .phone(signupReq.getPhone())
-                    .password(passwordEncoder.encode(signupReq.getPassword()))
-                    .build());
-        }
-    }
-//    public void signUp(UserReq info) {
-//        // 유저만 만들어서 저장. Role이 다르다!
-//            User user = User.builder()
-//                    .id(info.getId())
-//                    .nickname(info.getNickname())
-//                    .passwd(passwordEncoder.encode(info.getPasswd()))   //decoding how?
-//                    //   .roles(Collections.singletonList("ROLE_USER"))
-//                    .role("USER")
-//                    .pg(info.getPg())
-//                    .photo(info.getPhoto())
-//                    .build();
-//            userRepository.save(user);
-//    }
 
     @Override
     public UserLoginPostRes login(LoginReq loginReq) {
-        //logger.debug("로그인 메서드 진입");
         User user = userRepository.findByEmail(loginReq.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
         if (!passwordEncoder.matches(loginReq.getPassword(), user.getPassword())) {
@@ -79,7 +58,7 @@ public class UserServiceImpl implements UserService{
     public UserLoginPostRes editUser(String jwt, EditUserReq editUserReq){
         try{
             String email = jwtTokenProvider.getUserInfo(jwt);
-            User user = userRepository.findByEmail(email).get();
+            User user = userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 아이디입니다."));
             user.updateUserProfile(editUserReq);
             userRepository.save(user);
 
@@ -91,7 +70,10 @@ public class UserServiceImpl implements UserService{
             return resbody;
         }catch (Exception e){
             System.out.println(e);
-            return null;
+            UserLoginPostRes resbody = new UserLoginPostRes();
+            resbody.setJwt(jwt);
+            resbody.setEmail(null);
+            return resbody;
         }
     }
 
@@ -103,16 +85,16 @@ public class UserServiceImpl implements UserService{
 //        return BaseResponseBody.of(200, "Success Logout");
 //    }
 
-    @Override
-    public UserLoginPostRes getProfile(String token) {
-        String email = jwtTokenProvider.getUserInfo(token);
-
-        User member = userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 아이디입니다."));
-        UserLoginPostRes userLoginPostRes = new UserLoginPostRes();
-
-        userLoginPostRes.setEmail(email);
-        userLoginPostRes.setNickname(member.getNickname());
-        userLoginPostRes.setPhoneNum(member.getPhone());
-        return userLoginPostRes;
-    }
+//    @Override
+//    public UserLoginPostRes getProfile(String token) {
+//        String email = jwtTokenProvider.getUserInfo(token);
+//
+//        User member = userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 아이디입니다."));
+//        UserLoginPostRes userLoginPostRes = new UserLoginPostRes();
+//
+//        userLoginPostRes.setEmail(email);
+//        userLoginPostRes.setNickname(member.getNickname());
+//        userLoginPostRes.setPhoneNum(member.getPhone());
+//        return userLoginPostRes;
+//    }
 }
