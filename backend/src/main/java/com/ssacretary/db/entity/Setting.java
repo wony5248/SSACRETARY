10d405658482260885d9e2799e38308178e15dd5
jihyun9623 <jihyun9623@gmail.com>
2021-11-12 19:17:15 +0900

    Feat : editSetting API 구현 완료

--- backend/src/main/java/com/ssacretary/db/entity/Setting.java
+++ backend/src/main/java/com/ssacretary/db/entity/Setting.java
@@ -57,8 +57,6 @@ public class Setting {
     private LocalDateTime  updatedAt;
 
     public void updateSetting(EditSettingReq editSettingReq){
-        //키워드는 어떡하냐
-//        this.
         this.url=editSettingReq.getUrl();
         this.type=editSettingReq.getType();
         this.period= editSettingReq.getPeriod();
