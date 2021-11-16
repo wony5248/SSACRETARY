b2f5245e0dd464e77cb6061ded62c8ad3404855d
jihyun9623 <jihyun9623@gmail.com>
2021-11-16 10:43:01 +0900

    Fix : 세팅 수정하기 수정

--- backend/src/main/java/com/ssacretary/db/repository/SettingKeywordRepository.java
+++ backend/src/main/java/com/ssacretary/db/repository/SettingKeywordRepository.java
@@ -3,8 +3,12 @@ package com.ssacretary.db.repository;
 import com.ssacretary.db.entity.SettingKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 public interface SettingKeywordRepository extends JpaRepository<SettingKeyword,Integer> {
     List<SettingKeyword> findBySetting_SettingId(int settingId);
+
+    long deleteBySkId(int skId);
 }
