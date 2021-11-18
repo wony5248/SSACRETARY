888b05ef5d4f2299c5c7f346be72b385c3fd653d
OTKRyu <appn12@gmail.com>
2021-11-18 20:14:02 +0900

    style: container hegith 650px to 700px

--- frontend/src/components/Container/index.tsx
+++ frontend/src/components/Container/index.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 400px;
-  height: 650px;
+  height: 700px;
   display: flex;
   flex-direction: column;
   align-items: center;
